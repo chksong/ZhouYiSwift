087e32343201b29ea64eeea2fa9c3fc9900d153b
chksong <chksong@qq.com>
2015-12-10 11:27:22 +0800

    Initial Commit

---
+++ ZhouYiTests/ZhouYiTests.swift
@@ -0,0 +1,36 @@
+//
+//  ZhouYiTests.swift
+//  ZhouYiTests
+//
+//  Created by chksong on 15/12/10.
+//  Copyright © 2015年 chksong company. All rights reserved.
+//
+
+import XCTest
+@testable import ZhouYi
+
+class ZhouYiTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
